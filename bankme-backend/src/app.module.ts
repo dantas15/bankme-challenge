d9b8761e030237639fc4d3b09d8daa0a7a38effa
Gustavo Dantas <gusdantas15@gmail.com>
2024-05-10 07:28:25 -0300

    fix(assignor): add AssignorModule to app.module imports

--- bankme-backend/src/app.module.ts
+++ bankme-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PayableModule],
+  imports: [ConfigModule.forRoot(), AssignorModule, PayableModule],
   controllers: [AppController],
   providers: [AppService],
 })
