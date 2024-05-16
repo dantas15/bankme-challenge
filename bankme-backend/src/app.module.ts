2bd9bdcab56e4421e49180288a90efc2ec1290e4
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-16 09:03:04 -0300

    feat(auth): add auth resources

--- bankme-backend/src/app.module.ts
+++ bankme-backend/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PayableModule } from './payable/payable.module';
 import { AssignorModule } from './assignor/assignor.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AssignorModule, PayableModule],
+  imports: [ConfigModule.forRoot(), AssignorModule, PayableModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
