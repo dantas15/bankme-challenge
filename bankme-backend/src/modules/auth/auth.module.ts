cb0b84c473397630bedb5f2f08291e7309c3ec22
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-16 09:03:04 -0300

    feat(auth): add jwt and bcrypt

--- bankme-backend/src/modules/auth/auth.module.ts
+++ bankme-backend/src/modules/auth/auth.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from '../users/users.module';
+import { jwtConfig } from '../../config/auth.config';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConfig.secret,
+      signOptions: { expiresIn: '1m' }, // 1m = 1 minute
+    }),
+  ],
   controllers: [AuthController],
   providers: [PrismaService, AuthService],
 })
