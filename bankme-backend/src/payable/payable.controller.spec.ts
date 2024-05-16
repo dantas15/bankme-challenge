bc2460f5d7cae935e144dfcd665571c154a8324c
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-16 09:03:04 -0300

    test(payable): add crud tests for payable

--- bankme-backend/src/payable/payable.controller.spec.ts
+++ bankme-backend/src/payable/payable.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PayableController } from './payable.controller';
 import { PayableService } from './payable.service';
-import { createPayable } from '../helpers/faker/payable';
+import {
+  createPayable,
+  payables as mockedPayables,
+} from '../helpers/faker/payable';
 import { CreatePayableDto } from './dto/create-payable.dto';
 import { ArgumentMetadata, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { generateUuid } from '../helpers/faker/uuid';
+import { AssignorNotFoundException } from '../exceptions/assignor-not-found.exception';
 
 describe('PayableController', () => {
   let controller: PayableController;
+  let prisma: PrismaService;
+  let service: PayableService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,6 +23,13 @@ describe('PayableController', () => {
     }).compile();
 
     controller = module.get<PayableController>(PayableController);
+    service = module.get<PayableService>(PayableService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  afterEach(async () => {
+    // prevents error from multiple instances of prisma at the same time
+    await prisma.onModuleDestroy();
   });
 
   it('should be defined', () => {
@@ -31,14 +45,6 @@ describe('PayableController', () => {
       type: 'body',
       metatype: CreatePayableDto,
     };
-
-    it('should return the payable data if all information is right', async () => {
-      const validPayable = createPayable();
-
-      const result = await target.transform(validPayable, metadata);
-
-      expect(result).toMatchObject(validPayable);
-    });
 
     it('should return validation error messages if data is not valid', async () => {
       const wrongPayable = {
@@ -66,6 +72,182 @@ describe('PayableController', () => {
           'assignorId must be a UUID',
           'assignorId should not be empty',
         ]);
+      });
+    });
+
+    it('should return payable data if data is valid', async () => {
+      const assignorId = generateUuid();
+      const rawMockPayable = createPayable();
+      const mockPayable = {
+        value: rawMockPayable.value,
+        emissionDate: new Date(rawMockPayable.emissionDate),
+        assignorId,
+      };
+
+      service.assignorExists = jest
+        .fn()
+        .mockImplementation(
+          (assignorIdToSearch) => assignorIdToSearch === assignorId,
+        );
+      prisma.payable.create = jest.fn().mockReturnValue(mockPayable);
+
+      const result = await controller.create(mockPayable);
+
+      expect(service.assignorExists).toHaveBeenCalled();
+      expect(prisma.payable.create).toHaveBeenCalledWith({
+        data: {
+          ...mockPayable,
+          assignorId: undefined,
+          assignor: { connect: { id: assignorId } },
+        },
+      });
+      expect(result).toBe(mockPayable);
+    });
+
+    it('should not create payable and throw exception if assignorId does not exist', async () => {
+      const wrongAssignorId = generateUuid();
+      const rawMockPayable = createPayable();
+      const mockPayable = {
+        value: rawMockPayable.value,
+        emissionDate: new Date(rawMockPayable.emissionDate),
+        assignorId: wrongAssignorId,
+      };
+
+      service.assignorExists = jest
+        .fn()
+        .mockImplementation(
+          (assignorIdToSearch) =>
+            assignorIdToSearch === rawMockPayable.assignorId,
+        );
+      prisma.payable.create = jest.fn();
+
+      await controller.create(mockPayable).catch((exception) => {
+        expect(exception).toBeInstanceOf(AssignorNotFoundException);
+      });
+
+      expect(service.assignorExists).toHaveBeenCalledWith(wrongAssignorId);
+      expect(prisma.payable.create).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('find all payables', () => {
+    it('should return an array of payables', async () => {
+      prisma.payable.findMany = jest.fn().mockReturnValue(mockedPayables);
+      const result = await controller.findAll();
+
+      expect(prisma.payable.findMany).toHaveBeenCalledTimes(1);
+      expect(result).toBe(mockedPayables);
+    });
+  });
+
+  describe('find one payable', () => {
+    it('should show one payable based on the id', async () => {
+      const payableId = generateUuid();
+      const mockPayable = { ...createPayable(), id: payableId };
+      prisma.payable.findUnique = jest.fn().mockReturnValue(mockPayable);
+
+      const result = await controller.findOne(payableId);
+
+      expect(prisma.payable.findUnique).toHaveBeenCalledWith({
+        where: { id: payableId },
+      });
+      expect(result).toBe(mockPayable);
+    });
+  });
+
+  describe('update payable', () => {
+    it('should update payable if all data is valid and new assignorId is passed', async () => {
+      const assignorId = generateUuid();
+      const payableId = generateUuid();
+      const rawMockPayable = createPayable();
+      const mockPayable = {
+        id: payableId,
+        value: rawMockPayable.value,
+        emissionDate: new Date(rawMockPayable.emissionDate),
+        assignorId,
+      };
+
+      service.assignorExists = jest
+        .fn()
+        .mockImplementation(
+          (assignorIdToSearch) => assignorIdToSearch === assignorId,
+        );
+      prisma.payable.update = jest.fn().mockReturnValue(mockPayable);
+
+      const result = await controller.update(payableId, mockPayable);
+
+      expect(service.assignorExists).toHaveBeenCalledWith(assignorId);
+      expect(prisma.payable.update).toHaveBeenCalledWith({
+        where: { id: mockPayable.id },
+        data: {
+          value: mockPayable.value,
+          emissionDate: mockPayable.emissionDate,
+          assignor: { connect: { id: assignorId } },
+        },
+      });
+      expect(result).toBe(mockPayable);
+    });
+
+    it('should update payable if all data is valid and new assignorId is not passed', async () => {
+      const payableId = generateUuid();
+      const rawMockPayable = createPayable();
+      const mockPayable = {
+        value: rawMockPayable.value,
+        emissionDate: new Date(rawMockPayable.emissionDate),
+      };
+
+      service.assignorExists = jest.fn();
+      prisma.payable.update = jest.fn().mockReturnValue(mockPayable);
+
+      const result = await controller.update(payableId, mockPayable);
+
+      expect(service.assignorExists).not.toHaveBeenCalled();
+      expect(prisma.payable.update).toHaveBeenCalledWith({
+        where: { id: payableId },
+        data: mockPayable,
+      });
+      expect(result).toBe(mockPayable);
+    });
+
+    it('should not update payable and throw exception if new assignorId is not valid', async () => {
+      const assignorId = generateUuid();
+      const rawMockPayable = createPayable();
+      const mockPayable = {
+        value: rawMockPayable.value,
+        emissionDate: new Date(rawMockPayable.emissionDate),
+        assignorId: generateUuid(), // UUID that does not exist on assignor
+      };
+
+      service.assignorExists = jest
+        .fn()
+        .mockImplementation(
+          (assignorIdToSearch) => assignorIdToSearch === assignorId,
+        );
+      prisma.payable.update = jest.fn();
+
+      await controller
+        .update(generateUuid(), mockPayable)
+        .catch((exception) => {
+          expect(exception).toBeInstanceOf(AssignorNotFoundException);
+        });
+
+      expect(service.assignorExists).toHaveBeenCalled();
+      expect(service.assignorExists).toHaveReturnedWith(false);
+      expect(prisma.payable.update).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('remove payable', () => {
+    it('should delete payable', async () => {
+      const mockPayable = { ...createPayable(), id: generateUuid() };
+      prisma.payable.delete = jest.fn();
+
+      prisma.payable.findUnique = jest.fn().mockReturnValue(mockPayable);
+
+      await controller.remove(mockPayable.id);
+
+      expect(prisma.payable.delete).toHaveBeenCalledWith({
+        where: { id: mockPayable.id },
       });
     });
   });
