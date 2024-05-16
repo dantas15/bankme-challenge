669a0388eafa9c5be6357e21256b427271a75bfb
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-16 09:03:04 -0300

    feat(user): add user controller

--- bankme-backend/src/modules/user/user.controller.spec.ts
+++ bankme-backend/src/modules/user/user.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [PrismaService, UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- bankme-backend/src/modules/user/user.controller.ts
+++ bankme-backend/src/modules/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('integrations/user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
