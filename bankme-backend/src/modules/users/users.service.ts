1b33945a386d12e55208994496852d3d61ede74b
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-16 09:03:04 -0300

    feat(user): add findOne user

--- bankme-backend/src/modules/users/users.service.ts
+++ bankme-backend/src/modules/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findOne(id: string) {
+    return this.prismaService.user.findFirst({ where: { id } });
+  }
+}
