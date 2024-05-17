2a897b3cbb3055f077a4e6088a8e63516ba5968e
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-17 10:03:57 -0300

    feat(auth): add guard to all endpoints
    - add validation where all endpoints must have
    - add skip-auth decorator to use with public endpoints

---
+++ bankme-backend/src/modules/auth/auth.guard.ts
@@ -0,0 +1,49 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { jwtConfig } from 'src/config/auth.config';
+import { IS_SKIP_AUTH_KEY } from 'src/decorators/skip-auth.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    const skipAuth = this.reflector.getAllAndOverride<boolean>(
+      IS_SKIP_AUTH_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+    if (skipAuth) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: jwtConfig.secret,
+      });
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
--- bankme-backend/src/modules/auth/auth.module.ts
+++ bankme-backend/src/modules/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { PrismaService } from '../prisma/prisma.service';
 import { UserModule } from '../user/user.module';
 import { jwtConfig } from '../../config/auth.config';
 import { UserService } from '../user/user.service';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
@@ -17,6 +19,15 @@ import { UserService } from '../user/user.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [PrismaService, JwtService, UserService, AuthService],
+  providers: [
+    PrismaService,
+    JwtService,
+    UserService,
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
