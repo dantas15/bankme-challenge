486eb975a33f4f59b7d94bb6bff2bc095f7320c3
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-17 13:37:18 -0300

    fix(auth): add SkipAuth to signIn

--- bankme-backend/src/modules/auth/auth.controller.ts
+++ bankme-backend/src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { SkipAuth } from 'src/decorators/skip-auth.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @SkipAuth()
   @HttpCode(HttpStatus.OK)
   @Post()
   async signIn(@Body() signInDto: SignInDto) {
