085e8cbb709750fe0fb5cafcc9a9feb0404d0b15
Gustavo Dantas Galote <gusdantas15@gmail.com>
2024-05-17 13:32:05 -0300

    fix(user): add user.controller to user.module

--- bankme-backend/src/modules/user/user.module.ts
+++ bankme-backend/src/modules/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserController } from './user.controller';
 
 @Module({
+  controllers: [UserController],
   providers: [PrismaService, UserService],
   exports: [UserModule],
 })
