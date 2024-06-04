43063d4b651ab352c153a2027e51015cb6648d49
Gustavo Dantas <gusdantas15@gmail.com>
2024-06-03 23:37:30 -0300

    feat(auth): add loginSchema

--- apps/web/src/shared/schemas/user-schema.ts
+++ apps/web/src/shared/schemas/user-schema.ts
@@ -1,17 +1,25 @@
 import { z } from 'zod';
 
+const usernameSchema = z
+  .string()
+  .min(1, {
+    message: 'username is required',
+  })
+  .max(140, {
+    message: 'username must have less than 140 characters',
+  });
+const passwordSchema = z.string().min(1, {
+  message: 'password is required',
+});
+
 export const userSchema = z.object({
-  username: z
-    .string()
-    .min(1, {
-      message: 'username is required',
-    })
-    .max(140, {
-      message: 'username must have less than 140 characters',
-    }),
-  password: z.string().min(1, {
-    message: 'password is required',
-  }),
+  username: usernameSchema,
+  password: passwordSchema,
+});
+export const loginSchema = z.object({
+  login: usernameSchema,
+  password: passwordSchema,
 });
 
 export type User = z.infer<typeof userSchema>;
+export type LoginData = z.infer<typeof loginSchema>;
