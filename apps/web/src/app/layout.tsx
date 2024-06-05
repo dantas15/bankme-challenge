853dcf1e092ee7a78f3e0669385686eb763b2b3c
Gustavo Dantas <gusdantas15@gmail.com>
2024-06-05 11:15:47 -0300

    feat(toast): add toast

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins as FontSans } from 'next/font/google';
 import { cn } from '@/shared/lib/utils';
+import { Toaster } from '@/shared/components/ui/toaster';
 import './globals.css';
 import { Providers } from './providers';
 
@@ -32,6 +33,7 @@ export default function RootLayout({
         <div className="flex min-h-screen items-center justify-center w-full">
           <Providers>{children}</Providers>
         </div>
+        <Toaster />
       </body>
     </html>
   );
